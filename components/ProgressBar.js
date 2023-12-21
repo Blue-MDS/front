381237ef4a964eff61b7a9c94c777cff1ea7566c
Lucas Malatchoumy <malatchoumylucas.com>
2023-12-22 00:46:10 +0100

    refact(ProgressBar): change progress bar to stepper

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -2,44 +2,34 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 const ProgressBar = ({ currentStep, totalSteps }) => {
-  const progress = (currentStep / totalSteps) * 100;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.progressText}>
-        Étape {currentStep} sur {totalSteps}
-      </Text>
-      <View style={styles.progressBarContainer}>
-        <View style={[styles.progressBar, { width: `${progress}%` }]} />
-      </View>
-      
+      {Array.from({ length: totalSteps }).map((_, index) => (
+        <View key={index} 
+        style={[ styles.step, index + 1 === currentStep ? styles.activeStep : styles.inactiveStep ]} />
+      ))}   
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     marginVertical: 20,
   },
-  progressBarContainer: {
-    width: '90%',
-    height: 10,
+  step: {
+    width: 64,
+    height: 8,
+    marginHorizontal: 5,
+  },
+  activeStep: {
+    backgroundColor: 'black',
+  },
+  inactiveStep: {
     backgroundColor: '#e0e0e0',
-    borderRadius: 10,
-    overflow: 'hidden',
-  },
-  progressBar: {
-    height: '100%',
-    backgroundColor: '#000',
-    borderRadius: 10,
-  },
-  progressText: {
-    marginBottom: 5,
-    marginLeft: 'auto',
-    marginRight: 20
   },
 });
 
