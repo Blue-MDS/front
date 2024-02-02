329522068a620de516d9efe1179fbce454497714
Lucas Malatchoumy <malatchoumylucas.com>
2024-02-02 09:40:07 +0100

    fixup! refact(Authentication): save userInfos in expo secure store to refetch at launch

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -2,6 +2,7 @@ import React, { createContext, useReducer, useEffect, useMemo } from 'react';
 import * as SecureStore from 'expo-secure-store';
 import { API_URL } from '@env';
 import axios from 'axios';
+import { checkProfileCompletion } from '../services/userService';
 
 export const AuthContext = createContext();
 
@@ -10,6 +11,8 @@ const startState = {
   isSignout: false,
   userToken: null,
   errorMessage: '',
+  isProfileComplete: false,
+  isDailyGoalAccepted: false,
 };
 
 const authReducer = (prevState, action) => {
@@ -32,6 +35,8 @@ const authReducer = (prevState, action) => {
         ...prevState,
         isSignout: true,
         userToken: null,
+        isProfileComplete: false,
+        isDailyGoalAccepted: false,
       };
     case 'ERROR':
       return {
@@ -45,6 +50,16 @@ const authReducer = (prevState, action) => {
         isLoading: false,
         errorMessage: '',
       };
+    case 'COMPLETE_PROFILE':
+      return {
+        ...prevState,
+        isProfileComplete: true,
+      };
+    case 'ACCEPT_DAILY_GOAL':
+      return {
+        ...prevState,
+        isDailyGoalAccepted: true,
+      };
     default:
       return prevState;
   }
@@ -55,12 +70,18 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const bootstrapAsync = async () => {
-      let userToken;
+      let userToken, isProfileComplete, isDailyGoalAccepted;
       try {
         userToken = await SecureStore.getItemAsync('userToken');
+        console.log(await SecureStore.getItemAsync('userInfo'));
+        isProfileComplete = await SecureStore.getItemAsync('userInfo') ? JSON.parse(await SecureStore.getItemAsync('userInfo')).profile_complete : false;
+        isDailyGoalAccepted = await SecureStore.getItemAsync('isDailyGoalAccepted') === 'true';
       } catch (e) {
+        console.error(e);
       }
       dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+      if (isProfileComplete) dispatch({ type: 'COMPLETE_PROFILE' });
+      if (isDailyGoalAccepted) dispatch({ type: 'ACCEPT_DAILY_GOAL' });
     };
 
     bootstrapAsync();
@@ -71,30 +92,55 @@ export const AuthProvider = ({ children }) => {
       try {
         const response = await axios.post(`${API_URL}/users/login`, data);
         await SecureStore.setItemAsync('userToken', response.data.token);
+        const userInfo = JSON.stringify(response.data.user);
+        await SecureStore.setItemAsync('userInfo', userInfo);
         dispatch({ type: 'SIGN_IN', token: response.data.token });
+        console.log(userInfo);
+        if (userInfo.profile_complete) {
+          dispatch({ type: 'COMPLETE_PROFILE' });
+        }
       } catch (error) {
         console.log(error.response.data.message);
         dispatch({ type: 'ERROR', error: error.response.data.message });
       }
     },
     signOut: async () => {
+      await SecureStore.deleteItemAsync('dailyGoal');
       await SecureStore.deleteItemAsync('userToken')
+      await SecureStore.deleteItemAsync('profileIsCompleted');
+      await SecureStore.deleteItemAsync('isDailyGoalAccepted');
+      await SecureStore.deleteItemAsync('currentStep');
       dispatch({ type: 'SIGN_OUT' })
     },
     signUp: async (data) => {
       try {
         const response = await axios.post(`${API_URL}/users/subscribe`, data);
         await SecureStore.setItemAsync('userToken', response.data.token);
+        const userInfo = JSON.stringify(response.data.user);
+        await SecureStore.setItemAsync('userInfo', userInfo);
         dispatch({ type: 'SIGN_IN', token: response.data.token });
       } catch (error) {
-        dispatch({ type: 'ERROR', error: error.response.data.message });
+        if (error.response && error.response.data) {
+          dispatch({ type: 'ERROR', error: error.response.data.message });
+        } else {
+          dispatch({ type: 'ERROR', error: error.message });
+        }
       }
+    },
+    acceptDailyGoal: () => {
+      SecureStore.setItemAsync('isDailyGoalAccepted', 'true');
+      dispatch({ type: 'ACCEPT_DAILY_GOAL' });
+    },
+    completeProfile: () => {
+      dispatch({ type: 'COMPLETE_PROFILE' });
     },
     resetError: () => dispatch({type: 'RESET_ERROR'}),
     userToken: state.userToken,
     isLoading: state.isLoading,
     isSignout: state.isSignout,
     errorMessage: state.errorMessage,
+    isProfileComplete: state.isProfileComplete,
+    isDailyGoalAccepted: state.isDailyGoalAccepted,
   }), [state]);
 
   return (
---
+++ screens/Settings/Settings.js
@@ -0,0 +1,60 @@
+// Dans SettingsScreen.js
+import React from 'react';
+import { useContext } from 'react';
+import { View, Text, TouchableOpacity, SafeAreaView, SectionList, StyleSheet } from 'react-native';
+import CustomButon from '../../components/Button';
+import { AuthContext } from '../../contexts/AuthContext';
+
+const SettingsScreen = ({ navigation }) => {
+  const { signOut } = useContext(AuthContext);
+
+  const DATA = [
+    {
+      title: "Profil",
+      data: ["Paramètres du profil",],
+    },
+    {
+      title: "Objectif",
+      data: ["DailyGoal", "Notifications",],
+    },
+    {
+      title: "Security",
+      data: ["Confidentialité et sécurité"],
+    },
+    {
+      title: "Informations",
+      data: ["Nous contacter", "Partager l'application", "Supprimer mon compte"],
+    },
+  ];
+  return (
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.setting} onPress={() => navigation.navigate(item)}>
+            <Text style={{ padding: 10, fontSize: 18 }}>{item}</Text>
+          </TouchableOpacity>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={{ fontWeight: "bold", fontSize: 20, padding: 10 }}>{title}</Text>
+        )}
+      />
+      <CustomButon text="Déconnexion" onPress={() => signOut()} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: 'center',
+    marginHorizontal: 20,
+  },
+  setting: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#E1E1E1",
+  },
+});
+
+export default SettingsScreen;
