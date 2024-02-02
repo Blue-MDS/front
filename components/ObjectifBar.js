1cbb5861b18dc963f8bc7a16e93766f346e44102
Lucas Malatchoumy <malatchoumylucas.com>
2024-02-02 09:18:54 +0100

    feat(ObjectifBar): create bar to see user progression

---
+++ components/ObjectifBar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const ObjectifBar = ({ dailyGoal, totalConsumption }) => {
+  const progress = dailyGoal && totalConsumption ? (totalConsumption / dailyGoal) * 100 : 0;
+  const clampedProgress = Math.min(progress, 100);
+
+  const bubbleWidth = 50; // Définir la largeur de la bulle
+  let bubbleLeft = `${clampedProgress}%`;
+  if (clampedProgress < (bubbleWidth / 2)) {
+    bubbleLeft = '0%'; // Garder la bulle dans la vue quand le progrès est très faible
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.bubble, { left: bubbleLeft, transform: [{ translateX: -(bubbleWidth / 2) }]
+      }]}>
+        <Text style={styles.bubbleText}>{`${clampedProgress.toFixed(0)}%`}</Text>
+      </View>
+      <View style={styles.progressBarContainer}>
+        <View style={[styles.progressBar, { width: `${clampedProgress}%` }]} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    overflow: 'hidden',
+    position: 'relative',
+    paddingHorizontal: 20
+  },
+  progressBarContainer: {
+    marginTop: 10,
+    height: 12,
+    width: '100%',
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+  },
+  progressBar: {
+    borderRadius: 5,
+    height: '100%',
+    backgroundColor: 'black',
+  },
+  bubble: {
+    width: 50,
+    backgroundColor: 'black',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+  },
+  bubbleText: {
+    textAlign: 'center',
+    fontFamily: 'Poppins_400Regular',
+    color: 'white',
+  },
+});
+
+
